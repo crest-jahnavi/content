009e16cf92ade8268be000e9cd0f9046e56ead34
abaumgarten <abaumgarten@paloaltonetworks.com>
2021-02-11 11:00:10 +0200

    apply testing bucket patch & packs changes

--- Utils/trigger_upload_packs_to_production.sh
+++ Utils/trigger_upload_packs_to_production.sh
@@ -11,7 +11,7 @@ trigger_build_url="https://circleci.com/api/v2/project/github/demisto/content/pi
 
 post_data=$(cat <<-EOF
 {
-  "branch": "master",
+  "branch": "fix_hwp_upload_flow_slack",
   "parameters": {
     "bucket_upload": "true"
   }
